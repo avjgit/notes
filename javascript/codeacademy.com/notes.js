cdefd67027a13c7116870290afb188031b4a746c
AJ <avjmail@gmail.com>
2012-01-25 22:31:54 +0200

    Codecademy - Getting Started - Lesson 6, part 6

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -72,3 +72,14 @@ var word = "that";
 if( word === "that" ) {
   console.log( "word is equal to 'that'" );
 }
+
+var number = prompt( "pick a number between 1 and 10!" );
+if( number === 5 ) {
+  console.log( "your number is 5" );
+}
+else if ( number > 5 ) {
+  console.log( "your number is greater than 5" );
+}
+else if ( number < 5 ) {
+  console.log( "your number is less than 5" );
+}
