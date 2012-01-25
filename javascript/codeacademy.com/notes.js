7b7f66b205f9dac29b6fe7a6bda5de775129c2eb
AJ <avjmail@gmail.com>
2012-01-25 22:27:53 +0200

    Codecademy - Getting Started - Lesson 6, part 3

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -47,3 +47,13 @@ if (name === "Sam") {
 else {
   console.log("Your name isn't Sam!"); 
 };
+
+var name;
+// Prompt for the user's name here.
+name = prompt("what's your name?");
+
+if(name === "Sam") {
+  console.log("Your name is Sam!");
+} else{
+  console.log("Your name isn't Sam!");
+};
