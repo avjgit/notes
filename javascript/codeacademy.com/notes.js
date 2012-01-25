0cfa4ea51cc3a70712c5eaa1796d5bd145a35853
AJ <avjmail@gmail.com>
2012-01-25 22:22:19 +0200

    Codecademy - Getting Started - Lesson 6, part 2

---
+++ javascript/codeacademy.com/notes.js
@@ -0,0 +1,49 @@
+// Getting Started with Programming
+// lesson 1
+"AJ"
+"AJ".length
+2+2
+2*2
+AJ
+// lesson 2
+confirm("see my question?")
+alert("this is a alert!")
+// lesson 3
+myName
+var myName
+myName = 'AJ'
+myName
+var lastName = 'J'
+var myFullName = myName + lastName
+myFullName.length
+// lesson 4
+var number = 42
+number/2
+number%10
+var myString = 'hello'
+myString.substring(0, 2)
+var three = 'Andrey'.substring(0,3)
+'coding rules'.replace('coding', 'programming')
+three.toUpperCase()
+// lesson 5
+var today = 'Monday'
+var number = array[2]
+array[2] = number
+var numbers = [1, 2, 3];
+var days = [];
+days[0] = 'Monday';
+days[1] = 'Tuesday';
+days[2] = 'Wednesday';
+// lesson 6
+
+var number = 7;
+if( number === 7 ) {
+  console.log( "you typed a 7!" );
+}
+
+var name = "Annie";
+if (name === "Sam") {
+  console.log("Your name is Sam!"); }
+else {
+  console.log("Your name isn't Sam!"); 
+};
