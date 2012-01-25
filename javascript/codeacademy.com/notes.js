116ce3a687439781c90610a3cb831d36ec49033f
AJ <avjmail@gmail.com>
2012-01-25 22:28:16 +0200

    Codecademy - Getting Started - Lesson 6, part 4

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -57,3 +57,13 @@ if(name === "Sam") {
 } else{
   console.log("Your name isn't Sam!");
 };
+
+var number = prompt("Guess what number I'm thinking of between 1 and 10!");
+if(number === "7") {
+  console.log("You got it!");
+// Change the following line.
+} else if (number === "6") {
+  console.log("Close! Try guessing a little higher.");
+} else {
+  console.log("You were way off! Sorry...");
+}
