6b59448ec17db5a3a05052127fa5394a8f048e68
AJ <avjmail@gmail.com>
2012-02-06 22:57:22 +0200

    Javascript:Codeacademy:Primitives:39:24

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -966,3 +966,31 @@ var y = 2;
 if (y>x ) {
   console.log('y is larger than x');
 }
+
+
+
+
+// Write a statement that decides if the variable `i` is divisible by 3.  
+
+// for the numbers 1 through 20,
+for ( i=1; i<=20; i++) { 
+
+  // if the number is divisible by 3 *and* 5, write "FizzBuzz" 
+  if ((i%3 === 0) && (i%5 === 0) ) { 
+    console.log("FizzBuzz");
+  // if the number is divisible by 3 write "Fizz" 
+  } else if (i%3 === 0) {
+    console.log("Fizz");
+  // if the number is divisible by 5 write "Buzz" 
+  } else if (i%5 === 0) {
+    console.log("Buzz");
+  // otherwise, write just the number
+  } else {
+    console.log(i);
+  }
+}
+
+
+
+
+var numbers = [1, 2, 3];
