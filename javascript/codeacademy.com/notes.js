6dc3c80b4db7bf14b58612d5e6bb78c2d51d228a
AJ <avjmail@gmail.com>
2012-02-06 23:32:28 +0200

    Javascript:Codeacademy:Objects:33:12

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1171,3 +1171,46 @@ var species = dog['species'];
 // fill in the code to save the weight and age using bracket notation
 var weight = dog['weight'];
 var age = dog['age'];
+
+
+
+
+// help us make snoopy using literal notation
+// Remember snoopy is a "beagle" and is 10 years old.
+var snoopy = {
+    species: "beagle",
+    age: 10
+}
+
+// help make buddy using constructor notation
+// buddy is a "golden retriever" and is 5 years old
+var buddy = new Object();
+buddy.species = "golden retriever";
+buddy.age = 5;
+
+
+
+
+var bob = {
+  name: "Bob Smith",
+  age: 30
+};
+var susan = {
+  name: "Susan Jordan",
+  age: 25
+};
+// here we save Bob's information
+var name1 = bob.name;
+var age1 = bob.age;
+// finish this code by saving Susan's information
+var name2 = susan.name;
+var age2 = susan.age;
+
+
+
+
+var BMW = {
+    cost: "too much",
+    speed: 220,
+    country: "Germany"
+}
