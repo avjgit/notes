948505308927261a1154079ced0d6ad513401fb1
AJ <avjmail@gmail.com>
2012-02-06 21:35:23 +0200

    Javascript:Codeacademy:project:Dice:6:

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -877,13 +877,16 @@ if ( x > y){
 
 
 // pick a random number between 1 and 6 for our roll of the die
-var die = Math.floor(Math.random()*6 + 1);
+var die1 = Math.floor(Math.random()*6 + 1);
+var die2 = Math.floor(Math.random()*6 + 1);
 var score;
 
-// this time make an if statement so that if the roll is 1 the score is 0
-if (die === 1){
+// this time if either dice roll is 1 then score should be 0
+// Otherwise, score should be the sum of the two dice
+if (die1 === 1 || die2 === 1){
     score = 0;
 }else{
-    score = die;
+    score = die1 + die2;
 }
-console.log("You rolled a "+die+" for a score of "+score);
+
+console.log("You rolled a "+die1+" and a "+die2+" for a score of "+score);
