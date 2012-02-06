92df2b0967a17b36d846a19b5b05c1b05ed99360
AJ <avjmail@gmail.com>
2012-02-06 18:53:02 +0200

    JS Codecademy - functions - variables - 6:6

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -279,3 +279,27 @@ var sayHelloTo = function (name) {
 };
 
 sayHelloTo("AJ");
+
+
+
+
+/* Start global variable `greeting` scope */
+var greeting = "Ahoy";
+
+var greet = function () {
+  /* Start local variable `greeting` scope */
+  var greeting = "Hello";
+  
+  /* Start local variable `myName` scope */
+  var myName = prompt("Input your name: ");
+  
+  console.log(greeting + " " + myName);
+  
+  /* End local variables `myName` and `greeting` scope */
+};
+
+greet();
+
+// This will produce a `ReferenceError` since we are out
+// of the `myName` variable scope.
+console.log(myName);
