f2451a614972bba603e09a6fd0ea8d41d536e466
AJ <avjmail@gmail.com>
2012-02-06 20:06:48 +0200

    JS Codecademy - Olympic trials 6:3

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -572,3 +572,24 @@ for ( i = 0; i < raceTimes.length; i++ ) {
 }
 
 var averageTime = totalTime/ raceTimes.length;
+
+
+
+
+// runner times
+var carlos = [9.6,10.6,11.2,10.3,11.5];
+var liu = [10.6,11.2,9.4,12.3,10.1];
+var timothy = [12.2,11.8,12.5,10.9,11.1];
+
+// declare your function calculateTotal here
+var calculateTotal = function(raceTimes){
+    var totalTime = 0;
+    for (i=0;i<raceTimes.length;i++){
+        totalTime += raceTimes[i];
+    }
+    return totalTime;
+}
+
+var liuTotal = calculateTotal(liu);
+
+console.log(liuTotal);
