10e74fbfc039ec2965d5b244767815eac76d5cd3
AJ <avjmail@gmail.com>
2012-02-06 20:15:55 +0200

    JS Codecademy - Olympic trials 6:5

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -590,3 +590,17 @@ var calculateAverage = function (raceTimes) {
 };
 
 console.log(calculateAverage(timothy));
+
+
+
+
+var isQualified = function(averageTime){
+    if(averageTime >= 11.5){
+        console.log("Close, but you didn't make the cut.");
+    }else{
+        console.log("Welcome to the team, speedy!")
+    }
+}
+
+isQualified(11.3);
+isQualified(12.1);
