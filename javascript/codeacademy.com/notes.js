e9c75845999cc1ee58c2a1bfab2af98ae04b1970
AJ <avjmail@gmail.com>
2012-02-06 21:10:32 +0200

    Javascript:Codeacademy:Conditionals:29:19

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -755,3 +755,24 @@ switch (born) {
         result = "Born outside the USA";
     break;   
 }
+
+
+
+
+var born = prompt("What country were you born in?");
+var result;
+
+// write the whole switch statement 
+switch(born){
+    case "USA":
+        result = 1;
+    break;
+    
+    case "England":
+        result = 2;
+    break;
+    
+    default:
+        result = 3;
+    break;
+}
