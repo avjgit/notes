b67a538990e8696d400567d3aa6a38c76cc793af
AJ <avjmail@gmail.com>
2012-02-06 22:50:42 +0200

    Javascript:Codeacademy:Primitives:39:20

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -948,3 +948,9 @@ console.log("No semi-colons in loops or conditionals!");
 var number = 10;
 console.log("I think I am 10 out of " + number);
 "Hi" === "hi"
+console.log( 2 >= 1 ); // is greater than or equal to.
+console.log( 4 > 1 ); // is greater than.
+console.log( 0 === 0); // is equal to.
+console.log( 1 <= 1 ); // is less than or equal to.
+console.log( "Hi" !== "hi" ); // is not equal to.
+console.log( 0 < 1 ); // is less than.
