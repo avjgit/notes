5fea3e1e147ed35e6d68900397242dfc8bebadae
AJ <avjmail@gmail.com>
2012-02-06 23:21:05 +0200

    Javascript:Codeacademy:Objects:33:04

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -900,7 +900,7 @@ var calculateTotalCosts = function(salary, numWorkers, city) {
   var variableCosts = salary * numWorkers;
   
   if (city === "NYC") {
-    return fixedCosts + variableCosts + 30000;
+     fixedCosts + variableCosts + 30000;
   }
   else if (city === "BEJ") {
     return fixedCosts + variableCosts + 25000;
@@ -1100,3 +1100,32 @@ for (i=1; i<=20; i++) {
     console.log(i);
   }
 }
+
+
+
+
+var getReview = function (movie) {
+    switch(movie){
+        case "Matrix":
+            return "good trip out";
+        break;
+        case "Princess Bride":
+            return "awesome date night movie";
+        break;
+        case "Welcome to America":
+            return "Amjad's favorite";
+        break;
+        case"Remember the Titans":
+            return "love the sports";
+        break;
+        case "Why do I look like I'm 12?":
+            return "The Ryan and Zach story";
+        break;
+        case "Fighting Kangaroos in the wild": 
+            return "Token Australian movie for Leng";
+        break;
+        default:
+            return "I don't know!"
+        break;
+    }
+};
