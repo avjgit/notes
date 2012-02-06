9abf5fb5599f79b05d446ac53c20500e3e2f9bc1
AJ <avjmail@gmail.com>
2012-02-06 23:25:39 +0200

    Javascript:Codeacademy:Objects:33:08

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1143,3 +1143,16 @@ var Me = {
     age: 29,
     country: "Latvia"
 }
+// Our bob object again, but made using a constructor this time 
+var bob = new Object();
+bob.name = "Bob Smith";
+bob.age = 30;
+// Here is susan again, in literal notation
+var susan1 = {
+  name: "Susan Jordan",
+  age: 24
+};
+// Make a new susan2 object, using a constructor instead
+var susan2 = new Object();
+susan2.name = "Susan Jordan";
+susan2.age = 24;
