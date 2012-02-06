3f2296d98966508a1298eb7a3a982da78ebd5b9d
AJ <avjmail@gmail.com>
2012-02-06 21:32:23 +0200

    Javascript:Codeacademy:project:Dice:6:4

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -880,7 +880,10 @@ if ( x > y){
 var die = Math.floor(Math.random()*6 + 1);
 var score;
 
-// set score here equal to die
-var score = die;
-
+// this time make an if statement so that if the roll is 1 the score is 0
+if (die === 1){
+    score = 0;
+}else{
+    score = die;
+}
 console.log("You rolled a "+die+" for a score of "+score);
