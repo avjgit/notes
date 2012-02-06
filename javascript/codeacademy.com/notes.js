1a23e90c28d251394645fc700e4e43254dd438a7
AJ <avjmail@gmail.com>
2012-02-06 22:59:58 +0200

    Javascript:Codeacademy:Primitives:39:26

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1001,3 +1001,9 @@ var animals = ["cat", "dog", "monkey"];
 console.log(animals[0]);
 console.log(animals[1]);
 console.log(animals[2]);
+// Here is an array of multiples of 5. But is it correct?
+var multiplesOfFive = [5,10,15,22,25,30];
+
+// Test to see if a number from the array is NOT a true
+// multiple of five. Real multiples will return false.
+multiplesOfFive[3] % 5 !== 0;
