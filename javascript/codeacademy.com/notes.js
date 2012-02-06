d71398b1790e7ab07d329c81818efe6d8d80f716
AJ <avjmail@gmail.com>
2012-02-06 21:30:24 +0200

    Javascript:Codeacademy:project:Dice:6:2

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -872,3 +872,11 @@ if ( x > y){
 }else{
     result = 20;
 }
+
+
+
+
+// pick a random number between 1 and 6 for our roll of the die
+var die = Math.floor(Math.random()*6 + 1);
+
+console.log("You rolled a "+die);
