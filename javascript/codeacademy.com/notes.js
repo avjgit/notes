26b590fff1d9c01f1fdcd32e055425c8a91163a3
AJ <avjmail@gmail.com>
2012-02-06 18:04:40 +0200

    Codecademy

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -142,3 +142,81 @@ while(i<4 );
 for(i=1;i<21;i++){
     console.log(i);
 }
+
+for ( i=1; i<=20; i++) { 
+  
+  // if the number is divisible by 3, write "Fizz"
+  if (i%3==0 ) { 
+    console.log("Fizz");
+  }
+  
+  // otherwise, write just the number
+  else {
+      console.log(i);
+    
+  }
+}
+
+// for the numbers 1 through 20,
+for (i=1; i<=20; i++) { 
+  
+  // if the number is divisible by 3, write "Fizz"
+  if ( i % 3 === 0 ) { 
+    console.log("Fizz");
+  }
+  else if(i%5===0)
+  // if the number is divisible by 5, write "Buzz"
+  {
+      console.log("Buzz");    
+  }
+  
+  // otherwise, write just the number
+  else {
+    console.log(i);
+  }
+}
+
+for (i=1; i<=20; i++) { 
+  
+  if (i%3===0 && i%5===0){
+      console.log("FizzBuzz");
+  }
+  // if the number is divisible by 3, write "Fizz"
+  else if ( i % 3 === 0 ) { 
+    console.log("Fizz");
+  }
+  else if(i%5===0)
+  // if the number is divisible by 5, write "Buzz"
+  {
+      console.log("Buzz");
+  }
+  
+  // otherwise, write just the number
+  else {
+    console.log(i);
+  }
+}
+
+// Add an else statement in case the number is divisible by 5. 
+var upper_limit = prompt("count till...");
+// for the numbers 1 through 20,
+for (i=1; i<=upper_limit; i++) { 
+  
+  if (i%3===0 && i%5===0){
+      console.log("FizzBuzz");
+  }
+  // if the number is divisible by 3, write "Fizz"
+  else if ( i % 3 === 0 ) { 
+    console.log("Fizz");
+  }
+  else if(i%5===0)
+  // if the number is divisible by 5, write "Buzz"
+  {
+      console.log("Buzz");
+  }
+  
+  // otherwise, write just the number
+  else {
+    console.log(i);
+  }
+}
---
+++ javascript/codeacademy.com/readme.markdown
@@ -0,0 +1,13 @@
+### [Codecademy](http://www.codecademy.com)
++ Introduction  
+    1. Getting to Know You, Part I  
+    2. Confirm or Deny  
+    3. Variables  
+    4. Numbers n' Strings  
+    5. Editor and Arrays  
+    6. What If?  
+    7. Bringing It Up (and Down)  
+    8. While You Wait  
++ Functions  
+  Conditionals  
+  Review  
