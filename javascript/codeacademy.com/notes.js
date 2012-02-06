3538c37b6d5213a76cbd628b45707d050da91c39
AJ <avjmail@gmail.com>
2012-02-06 18:42:09 +0200

    JS Codecademy - functions - 1-4

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -244,3 +244,18 @@ var myFirstFunction =function(){
 }
 
 myFirstFunction();
+
+//1-4
+var fullName = "";
+var name;
+var firstLetter;
+var fixName= function(){
+    firstLetter = name.substring(0, 1);
+    name = firstLetter.toUpperCase() + name.substring(1);
+    fullName = fullName + " " + name;
+}
+name = prompt("Enter your first name (all in lower case):");
+fixName();
+name = prompt("Enter your second name (all in lower case):");
+fixName();
+console.log("And your full name is:" + fullName);
