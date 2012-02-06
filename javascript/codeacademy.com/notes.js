68d4af6bb052c9d5a1e113bdea22fa7e2d64b58b
AJ <avjmail@gmail.com>
2012-02-06 23:52:10 +0200

    Javascript:Codeacademy:Objects:33:18

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1273,3 +1273,23 @@ susan.setAge = setAge;
 
 // here, update Susan's age to 35 using the method
 susan.setAge(35);
+
+
+
+
+var rectangle = new Object();
+rectangle.length = 3;
+rectangle.width = 4;
+// here is our method to set the length
+rectangle.setLength = function (newLength) {
+  this.length = newLength;
+};
+// help by finishing this method
+rectangle.setWidth = function(newWidth){
+    this.width = newWidth;
+}
+  
+
+// here change the width to 8 and length to 6 using our new methods
+rectangle.setLength(6);
+rectangle.setWidth(8);
