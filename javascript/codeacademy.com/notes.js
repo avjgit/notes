c4069537a4d26918968437b746a6f139a05dbc94
AJ <avjmail@gmail.com>
2012-02-06 19:07:36 +0200

    JS Codecademy - functions - return - 5:5

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -335,3 +335,26 @@ var isEven = function (n) {
 console.log(isEven(1));
 console.log(isEven(2));
 console.log(isEven(999));
+
+
+
+
+var lost = [4, 8, 15, 16, 23, 42];
+var count = lost.length;
+
+var isLost = function (n) {
+  for (i=0;i<=count;i++ ) {
+    if ( n === lost[i]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+if ( isLost(12) ) {
+  console.log('12 is a lost number');
+}
+
+if ( isLost(16) ) {
+  console.log('16 is a lost number');
+}
