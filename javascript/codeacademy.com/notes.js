229c2142583140accaad6b84c7b720776428c9ec
AJ <avjmail@gmail.com>
2012-02-06 19:47:04 +0200

    JS Codecademy - Functions, review: Params 4:2

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -511,3 +511,15 @@ var volume = function (w, l, h ) {
 }
 
 volume(2, 3, 4);
+
+
+
+
+// Change the argument name from x to n
+var cube = function (n) {
+  return n * n * n;
+}
+
+// When you call cube with the new argument name, it
+// should still return the same result.
+cube(5);
