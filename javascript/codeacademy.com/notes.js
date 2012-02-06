d98091a412ff96cebc18774d753761cd962f90ae
AJ <avjmail@gmail.com>
2012-02-06 19:44:31 +0200

    JS Codecademy - Functions, review: Params 4:1

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -496,3 +496,18 @@ var cube = function(x) {
 }
 
 cube(7);
+
+
+
+
+var multiply = function (x, y) {
+   return x * y;
+}
+
+multiply(2, 5);
+
+var volume = function (w, l, h ) {
+  return w * l * h;
+}
+
+volume(2, 3, 4);
