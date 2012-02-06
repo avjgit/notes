9f9b8c0380235bc176ec7e21bc92916b115f3528
AJ <avjmail@gmail.com>
2012-02-06 19:41:42 +0200

    JS Codecademy - Functions, review 1

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -481,3 +481,18 @@ var taxiFare = function (milesTraveled, hourOfDay) {
   return cost;
 };
 var tripCost =  taxiFare(5, 2);
+
+
+
+
+// Accepts a number x as input and returns its square
+var square = function(x) {
+  return x * x;
+}
+
+// Accepts a number x as input and returns its cube
+var cube = function(x) {
+  return x * square(x);
+}
+
+cube(7);
