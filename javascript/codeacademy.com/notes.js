4be7dc35b9fe4a933479fcaac96f142ae5f5c596
AJ <avjmail@gmail.com>
2012-02-06 23:37:06 +0200

    Javascript:Codeacademy:Objects:33:14

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1229,3 +1229,18 @@ var multiply = function(x, y){
     return x*y;
 }
 multiply(2,3);
+
+
+
+
+// here is bob again, with his usual properties
+var bob = new Object();
+bob.name = "Bob Smith";
+bob.age = 30;
+// this time we have added a method, setAge
+bob.setAge = function (newAge){
+  bob.age = newAge;
+};
+// here we set bob's age to 40
+bob.setAge(20);
+// bob's feeling old.  Use our method to set bob's age to 20
