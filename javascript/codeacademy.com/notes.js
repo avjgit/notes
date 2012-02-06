508b880a4e6ff5492dab681bdb18a8e881310300
AJ <avjmail@gmail.com>
2012-02-06 19:05:14 +0200

    JS Codecademy - functions - return - 5:4

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -320,3 +320,18 @@ var identity = function (x) {
   return x;
 };
 identity();
+
+
+
+
+var isEven = function (n) {
+  if (n % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(isEven(1));
+console.log(isEven(2));
+console.log(isEven(999));
