45d60d7384fd8b1c074477e2f581f3d39f4ca082
AJ <avjmail@gmail.com>
2012-02-06 19:59:40 +0200

    JS Codecademy - Olympic trials 6:1

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -551,3 +551,16 @@ var volume = function (w, l, h) {
 
 console.log(volume(2, 3, 4));
 console.log(area);
+
+
+
+
+// 1. prompt for name
+var runner = prompt("What is your name?");
+
+// 2. write condition to ensure runner is not empty
+    if (runner.length > 0 ) { 
+	  console.log("Welcome to the Olympic tryouts, " + runner);
+  } else {
+	  console.log("Name cannot be blank.");
+  }
