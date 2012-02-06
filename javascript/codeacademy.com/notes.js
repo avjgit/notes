b394943e1d89e539f64f0934283e2b35a19c7023
AJ <avjmail@gmail.com>
2012-02-06 19:24:45 +0200

    JS Codecademy - functions - parameters - 7:6

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -455,5 +455,7 @@ power(2, 2);
 var power = function(base, exponent){
     if(exponent === 0){
         return 1;
-    }       
+    }    else{
+        return base * power(base, exponent-1);
+    }
 }
