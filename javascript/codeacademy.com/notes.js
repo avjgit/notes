7e1fb7eea05c6423a0d6e0f6d490d6188e89cd61
AJ <avjmail@gmail.com>
2012-02-06 23:34:54 +0200

    Javascript:Codeacademy:Objects:33:13

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1214,3 +1214,18 @@ var BMW = {
     speed: 220,
     country: "Germany"
 }
+
+
+
+
+// Accepts a number x as input and returns its square
+var square = function (x) {
+  return x * x;
+};
+
+// Write the function multiply below
+// It should take two parameters and return the product
+var multiply = function(x, y){
+    return x*y;
+}
+multiply(2,3);
