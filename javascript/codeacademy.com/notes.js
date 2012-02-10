14ed65c5504d540c4dd152ca6d19e4bf7f1cd828
AJ <avjmail@gmail.com>
2012-02-10 03:35:26 +0200

    Javascript: Codecademy: Objects: section 6/6: exercise 4/4

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1486,3 +1486,19 @@ function Circle (radius) {
     }
 };
 
+
+
+
+// 3 lines required to make the iliad
+var harry_potter = new Object();
+harry_potter.length = 350;
+harry_potter.author = "J.K. Rowling";
+
+// A custom constructor for book
+function Book (length, author) {
+    this.length = length;
+    this.author = author;
+}
+
+// Use our new constructor to make the_hobbit in one line
+var the_hobbit = new Book(320, "J.R.R. Tolkien")
