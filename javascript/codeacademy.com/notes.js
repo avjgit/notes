babeec3eb3453275cfe7be369b2750825188232a
AJ <avjmail@gmail.com>
2012-02-10 03:32:08 +0200

    Javascript: Codecademy: Objects: section 6/6: exercise 4/2

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1456,3 +1456,17 @@ var spencer = {
 var spencer2 = new Object();
 spencer2.age = 22;
 spencer2.country = "United States";
+
+
+
+
+var snoopy = new Object();
+snoopy.species = "beagle";
+snoopy.age = 10;
+
+// save Snoopy's age and species into variables
+// use dot notation for snoopy's species
+var species = snoopy.species;
+    
+// use bracket notation for snoopy's age
+var age = snoopy["age"];
