fa387423fc3a8cfbd11062350b1e67daa17cc25f
AJ <avjmail@gmail.com>
2012-02-10 02:54:41 +0200

    Javascript: Codecademy: Objects: section 4: exercise 6/2

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1319,3 +1319,17 @@ var bob = new Object();
 bob.name = "Bob Smith";
 // add bob's age here and set it equal to 20
 bob.age = 20;
+
+
+
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+// help us make george, whose name is "George Washington" and age is 275
+var george = new Person("George Washington", 275);
