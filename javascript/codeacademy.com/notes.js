764abac698d8ff9b2bdccb99bb04a606075a112f
AJ <avjmail@gmail.com>
2012-02-10 03:29:21 +0200

    Javascript: Codecademy: Objects: section 6/6: exercise 4/1

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1443,3 +1443,16 @@ var alice = new Person("Alice", 30);
 var billy = new Person("Billy", 25);
 
 console.log("The older person is "+olderAge(alice, billy));
+
+
+
+
+var spencer = {
+  age: 22,
+  country: "United States"
+};
+
+// make spencer2 here with constructor notation
+var spencer2 = new Object();
+spencer2.age = 22;
+spencer2.country = "United States";
