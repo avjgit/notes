2b3a25694a6024a5b3a8c043ae89a8b704fa3809
AJ <avjmail@gmail.com>
2012-02-10 02:56:07 +0200

    Javascript: Codecademy: Objects: section 4: exercise 6/3

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1333,3 +1333,17 @@ var bob = new Person("Bob Smith", 30);
 var susan = new Person("Susan Jordan", 25);
 // help us make george, whose name is "George Washington" and age is 275
 var george = new Person("George Washington", 275);
+
+
+
+
+function Cat(age, color) {
+  this.age = age;
+  this.color = color;
+}
+
+// make a Dog constructor here
+function Dog(age, name){
+    this.age = age;
+    this.name = name;
+}
