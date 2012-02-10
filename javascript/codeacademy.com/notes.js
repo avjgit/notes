685f2e364108d34d019b41ee19797ab3108d2464
AJ <avjmail@gmail.com>
2012-02-10 03:49:00 +0200

    Javascript: Codecademy: Objects: project Contacts: part 6/3

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1533,7 +1533,11 @@ function Person (
 }
 var bob  = new Person("Bob", "Jones", "(650) 777 - 7777", "bob.jones@example.com");
 var mary = new Person("Mary", "Johnson", "(650) 888 - 8888", "mary.johnson@example.com");
-var contacts = new Array();
-contacts[0] = bob;
-contacts[1] = mary;
+var contacts = [bob, mary];
 console.log(contacts[1].phoneNumber);
+// printPerson added here
+var printPerson = function(person){
+    console.log(person.firstName + ' ' + person.lastName);
+}
+printPerson(contacts[0]);
+printPerson(contacts[1]);
