ed494fe62e57ce1598e4be8028069dd55e647ac8
AJ <avjmail@gmail.com>
2012-02-10 02:58:44 +0200

    Javascript: Codecademy: Objects: section 4: exercise 6/4

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1347,3 +1347,18 @@ function Dog(age, name){
     this.age = age;
     this.name = name;
 }
+
+
+
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  // default property
+  this.species = "Homo Sapiens";
+}
+
+var sally = new Person("Sally Bowles", 39)
+var holden = new Person("Holden Caulfield", 16)
+console.log("sally's species is " + sally.species + " and she is " + sally.age);
+console.log("holden's species is " + holden.species + " and he is " + holden.age);
