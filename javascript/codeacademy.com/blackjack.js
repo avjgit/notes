dc25b1821148a83aa3cde0c38e66d7e47dcfdad5
AJ <avjmail@gmail.com>
2012-02-11 20:45:42 +0200

    Javascript: Codeacademy: project Blackjack: 1-2: 7/3

--- javascript/codeacademy.com/blackjack.js
+++ javascript/codeacademy.com/blackjack.js
@@ -1,11 +1,23 @@
-﻿// Define a function called deal
-// It should return a random number between 1 and 52
-var deal = function(){
-    var card = Math.floor(Math.random() * 52 + 1);
+﻿// Our deal function will return a random card
+function deal() {
+    card = Math.floor(Math.random()*52+1);
     return card;
 }
 
-// Declare two variables 
-// For both variables, assign values gotten by calling the function
+// Deal out our first hand
 var card1 = deal();
 var card2 = deal();
+
+// Make a getValue function here, which should convert a card to
+// the value that card is worth
+var getValue = function(card){
+    return card;
+}
+
+// Our score function converts our cards to a score
+var score = function () {
+    return getValue(card1) + getValue(card2);
+};
+
+console.log("You have cards " + card1 + " and " + card2 +
+        " for a score of " + score());
