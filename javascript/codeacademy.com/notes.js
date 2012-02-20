20ac232f1f8ce681452b252b877ae7116b8bc960
AJ <avjmail@gmail.com>
2012-02-20 22:07:50 +0200

    Javascript: Codeacademy: Objects II: Section 1: part of 5: 2

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1597,6 +1597,9 @@ var james = {
 function Person(job, married) {
     this.job = job;
     this.married = married;
+    this.speak = function(){
+        console.log("Hello!");
+    }    
 }
 
 // create a "gabby" object using the Person constructor!
