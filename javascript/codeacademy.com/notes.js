7283a3ecd5c7d3c1549801c981d3147049ffd719
AJ <avjmail@gmail.com>
2012-02-20 22:04:30 +0200

    Javascript: Codeacademy: Objects II: Section 1: part of 5: 1

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1584,3 +1584,20 @@ add(
     email,
     telephone
 );
+
+
+
+
+var james = {
+    // add properties to this object!
+    job:"programmer",
+    married:false
+};
+
+function Person(job, married) {
+    this.job = job;
+    this.married = married;
+}
+
+// create a "gabby" object using the Person constructor!
+var gabby = new Person("student", true);
