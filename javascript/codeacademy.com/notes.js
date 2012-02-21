f25e0c8ed12f982adf931d1d14d2737216fc5826
AJ <avjmail@gmail.com>
2012-02-21 07:56:19 +0200

    Javascript: Codeacademy: Objects II: Section 4/6: ex 6/6

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1815,3 +1815,31 @@ Emperor.prototype = new Penguin();
 // create an "emperor" object and print the number of legs it has
 var emperor = new Emperor("Big Emperor");
 console.log(emperor.numLegs);
+
+
+
+
+// original classes
+function Animal(name, numLegs) {
+    this.name = name;
+    this.numLegs = numLegs;
+    this.isAlive = true;
+}
+function Penguin(name) {
+    this.name = name;
+    this.numLegs = 2;
+}
+function Emperor(name) {
+    this.name = name;
+    this.saying = "Waddle waddle";
+}
+
+// set up the prototype chain
+Penguin.prototype = new Animal();
+Emperor.prototype = new Penguin();
+
+var myEmperor = new Emperor("Jules");
+
+console.log( myEmperor.saying ); // should print "Waddle waddle"
+console.log( myEmperor.numLegs ); // should print 2
+console.log( myEmperor.isAlive ); // should print true
