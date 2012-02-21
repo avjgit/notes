2f72793589748018cbd4c482ee7b2683659d200c
AJ <avjmail@gmail.com>
2012-02-21 07:43:12 +0200

    Javascript: Codeacademy: Objects II: Section 4/6: ex 1/5

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1779,3 +1779,19 @@ Cat.prototype.meow = function(){
 cheshire.meow();
 gary.meow();
 
+
+
+// create your Animal class here
+function Animal(name, numLegs){
+    this.name = name;
+    this.numLegs = numLegs;
+}
+
+// create the sayName method for Animal
+Animal.prototype.sayName = function(){
+    console.log("Hi my name is " + this.name);
+}
+
+// provided code to test above constructor and method
+var penguin = new Animal("Captain Cook", 2);
+penguin.sayName();
