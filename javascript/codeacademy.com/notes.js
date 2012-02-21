87da7def9df69ac0a7c4e7c117f4b909d2d012cc
AJ <avjmail@gmail.com>
2012-02-21 07:27:35 +0200

    Javascript: Codeacademy: Objects II: Section 2: ex of 5: 5

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1680,3 +1680,8 @@ var nyc = {
 for (var property in nyc ){
     console.log(property);
 }
+
+// write a for-in loop to print the value of nyc's properties
+for (var property in nyc ){
+    console.log(nyc[property]);
+}
