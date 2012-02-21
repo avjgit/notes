b0e86c8ca304d319e40a0727c8236bf65f422169
AJ <avjmail@gmail.com>
2012-02-21 07:31:04 +0200

    Javascript: Codeacademy: Objects II: Section 3/6: ex 1/5

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1685,3 +1685,20 @@ for (var property in nyc ){
 for (var property in nyc ){
     console.log(nyc[property]);
 }
+
+
+
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+};
+
+// Let's make bob again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 35);
+
+// make your own class here
+function Circle(radius){
+    this.radius = radius;
+}
