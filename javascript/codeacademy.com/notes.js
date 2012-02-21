8fed52cf19b415a5fdb21dcfa292fdcefbdd582f
AJ <avjmail@gmail.com>
2012-02-21 07:26:13 +0200

    Javascript: Codeacademy: Objects II: Section 2: ex of 5: 4

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1665,3 +1665,18 @@ else {
     suitcase.shorts = "hacky";
     console.log(suitcase.shorts);
 }
+
+
+
+
+var nyc = {
+    fullName: "New York City",
+    mayor: "Michael Bloomberg",
+    population: 8000000,
+    boroughs: 5
+};
+
+// write your for-in loop here
+for (var property in nyc ){
+    console.log(property);
+}
