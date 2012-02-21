905ba63441a6eca0d18750c659c45eba7bfeffc2
AJ <avjmail@gmail.com>
2012-02-21 07:14:15 +0200

    Javascript: Codeacademy: Objects II: Section 2: ex of 5: 1

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -1624,3 +1624,16 @@ function Person(job, married) {
 
 // create a "gabby" object using the Person constructor!
 var gabby = new Person("student", true);
+
+
+
+
+// complete these definitions so that they will have
+// the appropriate types
+var anObj = { job: "I'm an object!" };
+var aNumber = 42;
+var aString = "I'm a string!";
+
+console.log(typeof anObj  ); // should print "object"
+console.log(typeof aNumber  ); // should print "number"
+console.log(typeof aString  ); // should print "string"
