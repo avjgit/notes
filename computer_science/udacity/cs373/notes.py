d0d69f40f16d7c2f9f6fdd5e84e99d48e46382be
AJ <avjmail@gmail.com>
2012-02-25 21:40:20 +0200

    udacity cs373 unit 1 ex 24/36

--- computer_science/udacity/cs373/notes.py
+++ computer_science/udacity/cs373/notes.py
@@ -284,11 +284,10 @@ def move(p, U):
         s = s + pUndershoot * p[(i-U+1) % len(p)]
         q.append(s)
     return q
-#
-# ADD CODE HERE
-#
+
+for i in range(1000):
 p = move(p,1)
-p = move(p,1)
+
 print p
 
 
