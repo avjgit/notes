11ec91b5870f5a684dc0909ad9d6a0611b0fa762
AJ <avjmail@gmail.com>
2012-02-25 13:47:20 +0200

    udacity cs373 unit 1 ex 12/36 init

--- computer_science/udacity/cs373/notes.py
+++ computer_science/udacity/cs373/notes.py
@@ -77,7 +77,26 @@ print sum(p)
 
 
 # Sense Function - Question  
+#Modify the code below so that the function sense, which 
+#takes p and Z as inputs, will output the NON-normalized 
+#probability distribution, q, after multiplying the entries 
+#in p by pHit or pMiss according to the color in the 
+#corresponding cell in world.
 
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    #
+    #ADD YOUR CODE HERE
+	#
+    return q
+
+print sense(p,Z)
 
 # Normalized Sense Function - Question  
 
