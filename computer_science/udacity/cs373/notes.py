51f6dd8cb2b7af8f8c48d9cd9a515bd589fae349
AJ <avjmail@gmail.com>
2012-02-25 16:22:35 +0200

    udacity cs373 unit 1 ex 13/36 init

--- computer_science/udacity/cs373/notes.py
+++ computer_science/udacity/cs373/notes.py
@@ -106,6 +106,27 @@ def sense(p, Z):
 print sense(p,Z)
 
 # Normalized Sense Function - Question  
+#Modify your code so that it normalizes the output for 
+#the function sense. This means that the entries in q 
+#should sum to one.
+
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    sumq = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / sumq
+    return q
+print sense(p,Z)
 
 
 # Test Sense Function - Question  
