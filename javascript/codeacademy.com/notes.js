11489fb34210385e31debc52063f2f3175fbaf7f
AJ <avjmail@gmail.com>
2012-02-29 21:41:53 +0200

    Javascript: Codeacademy: For/While Loops: Section 1/6: ex 4/5

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -2071,3 +2071,33 @@ hongKong.tourism = "shop til you drop! eat til you sleep!";
 
 
 
+var australia = {
+    hemisphere: "southern",
+    population: function(newArrivals) {
+        return 20000000 + newArrivals;
+    }
+};
+
+var australia2 = new Object();
+australia2.hemisphere = "southern";
+australia2.population = function(newArrivals){
+    return 20000000 + newArrivals;
+};
+
+// create your objects and methods here
+var me = {
+    planet: "Earth",
+    age: function(yearBorn){
+        return 2012 - yearBorn;
+    }
+}
+
+var jeremyLin = new Object();
+jeremyLin.team = "Knicks";
+jeremyLin.playGame = function(opponent){
+    return "Tonight the Knicks square off against the " + opponent;
+}
+
+
+
+
