035aaa2d9c99b39d79642813e8b62301afd22ebb
AJ <avjmail@gmail.com>
2012-03-03 22:10:47 +0200

    saas c3 homework 1-1 palindrome init

---
+++ computer_science/stanford/saas/hw1_1__palindrom__wordcount.rb
@@ -0,0 +1,25 @@
+﻿# NOTE:  For all questions involving words or strings, you may assume that the 
+# definition of a "word" is "a sequence of characters whose boundaries are matched by 
+# the \b construct in Ruby regexps."
+# Part 1: fun with strings
+# (a) Write a method that determines whether a given word or phrase is a palindrome, 
+# that is, it reads the same backwards as forwards, ignoring case, punctuation, and 
+# nonword characters.  (a "nonword character" is defined for our purposes as "a 
+# character that Ruby regexps would treat as a nonword character".)  Your solution 
+# shouldn't use loops or iteration of any kind.  You will find regular-expression syntax 
+# very useful; it's reviewed briefly in the book, and the website rubular.com lets you try 
+# out Ruby regular expressions "live".  Methods you might find useful (which you'll 
+# have to look up in Ruby documentation, ruby-doc.org) include: String#downcase, 
+# String#gsub, String#reverse
+# Suggestion: once you have your code working, consider making it more beautiful by 
+# using techniques like method chaining, as described in ELLS 3.2.
+# Examples:
+# palindrome?("A man, a plan, a canal -- Panama")  #=> true
+# palindrome?("Madam, I'm Adam!")  # => true
+# palindrome?("Abracadabra")  # => false (nil is also ok)
+
+def palindrome?(string)
+  puts 'hello'
+end
+
+palindrome?('sadf')
