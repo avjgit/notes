6fd22e85ea5e80dffcea61a7d1fef14d6b96794f
AJ <avjmail@gmail.com>
2012-03-04 08:19:01 +0200

    udacity cs101 unit1 re-org

--- computer_science/stanford/saas/hw1_1__palindrom__wordcount.rb
+++ computer_science/stanford/saas/hw1_1__palindrom__wordcount.rb
@@ -24,3 +24,22 @@ def palindrome?(string)
   string[0..l/2].reverse ==				#compare first half to second
   string[l/2..l]
 end
+
+# (b) Given a string of input, return a hash whose keys are words in the string and 
+# whose values are the number of times each word appears.  Don't use forloops.  Nonwords should be ignored.  Case shouldn't matter.  A word is defined as a 
+# string of characters between word boundaries.  (Hint: the sequence \b in a Ruby 
+# regexp means "word boundary".) 
+# Example: 
+# count_words("A man, a plan, a canal -- Panama")
+# # => {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 
+# 'plan' => 1}
+# count_words "Doo bee doo bee doo"  # => {'doo' => 3, 'bee' => 2}
+def count_words(string)
+
+	array = string.split(/\b/)
+	# array.delete(/\p{Punctuation}/)
+	# array.delete_if {|word| ~= /\p{Punctuation}/}
+	array.delete_if {|word| word == ","}
+end
+
+puts count_words("a man, a plan, a canal -- Panama")
