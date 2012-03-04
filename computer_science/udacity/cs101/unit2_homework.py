330ee3ef59566aca39e55a58f12199ee4b884e54
AJ <avjmail@gmail.com>
2012-03-04 18:37:59 +0200

    udacity cs101 unit2 homework 7/7 init

--- computer_science/udacity/cs101/unit2_homework.py
+++ computer_science/udacity/cs101/unit2_homework.py
@@ -88,3 +88,21 @@ def countdown(count):
 # Make sure your procedure has a return statement.
 
 def find_last():    
+
+# 7/7
+#Define a procedure,
+#print_multiplication_table,
+#that takes as input a positive whole
+#number, and prints out a multiplication,
+#table showing all the whole number
+#multiplications up to and including the
+#input number. The order in which the
+#equations are printed must match exactly.
+
+#print_multiplication_table(2)
+#1 * 1 = 1
+#1 * 2 = 2
+#2 * 1 = 2
+#2 * 2 = 4
+
+def print_multiplication_table(n):
