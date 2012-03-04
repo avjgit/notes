f2525b9f41ae781b591c3e3ae6270031f510cc62
AJ <avjmail@gmail.com>
2012-03-04 14:51:51 +0200

    udacity cs101 unit2 homework 6/7 init

--- computer_science/udacity/cs101/unit2_homework.py
+++ computer_science/udacity/cs101/unit2_homework.py
@@ -71,7 +71,20 @@ def median(a, b, c):
 # followed by Blastoff! 
 
 def countdown(count):
-	while count > 1:
+	while count >= 1:
 		print count
 		count = count - 1
 	print "Blastoff!"
+
+# 6/7
+# Define a procedure, find_last, that takes as input
+# two strings, a search string and a target string,
+# and outputs the last position in the search string
+# where the target string appears, or -1 if there
+# are no occurences.
+#
+# Example: find_last('aaaa', 'a') returns 3
+
+# Make sure your procedure has a return statement.
+
+def find_last():    
