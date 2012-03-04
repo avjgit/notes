5bb909d6b9f3217f07fdb612462a3aa916c27171
AJ <avjmail@gmail.com>
2012-03-04 08:26:40 +0200

    udacity cs101 unit2 toc

---
+++ computer_science/udacity/cs101/unit2.py
@@ -0,0 +1,30 @@
+# Introduction
+# Motivating Procedures
+# Introducing Procedures
+# Procedure Code - Question
+# Output - Question
+# Return Statement - Question
+# Dave Sebastian And Junior
+# Using Procedures
+# Inc Procedure - Question
+# Sum Procedure - Question
+# Sum Procedure With A Return Statement - Question
+# Square - Question
+# Sum Of Three - Question
+# Abbaize - Question
+# Find Second - Question
+# Equality Comparisons - Question
+# If Statements - Question
+# Is Friend - Question
+# More Friends - Question
+# Or
+# Biggest - Question
+# While Loops - Question
+# While Loops 2 - Question
+# Print Numbers - Question
+# Factorial - Question
+# Break - Question
+# Multiple Assignment
+# Multiple Assignment - Question
+# No Links - Question
+# Print All Links - Question
