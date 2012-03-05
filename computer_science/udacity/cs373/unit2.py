a2225585faabf12dc1f9fb33ad9dc2e196c70be6
AJ <avjmail@gmail.com>
2012-03-05 23:48:03 +0200

    udacity cs373 unit2 25/34

--- computer_science/udacity/cs373/unit2.py
+++ computer_science/udacity/cs373/unit2.py
@@ -150,5 +150,8 @@ sig = 10000
 #and the variance in a list [mu, sig]. 
 
 # Insert code here
+for i in range(len(measurements)):
+    mu, sig = update(mu, sig, measurements[i], measurement_sig)
+    mu, sig = predict(mu, sig, motion[i], motion_sig)
 
 print [mu, sig]
