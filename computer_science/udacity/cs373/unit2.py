6d563451c4018ccdc11a719748913e4ff33355cd
AJ <avjmail@gmail.com>
2012-03-05 23:43:06 +0200

    udacity cs373 unit2 25/34 init

--- computer_science/udacity/cs373/unit2.py
+++ computer_science/udacity/cs373/unit2.py
@@ -114,11 +114,41 @@ print update(10.,8.,13., 2.)
 m_new = m + u
 sigma_new = sigma + other_sigma
 
+
 # Write a program that will predict your new mean
 # and variance given the mean and variance of your 
 # prior belief and the mean and variance of your 
 # motion. 
 def predict(mean1, var1, mean2, var2):
-    new_mean =
-    new_var =
+    new_mean = mean1 + mean2
+    new_var =  var1  + var2
     return [new_mean, new_var]
+
+#25
+# Write a program that will iteratively update and
+# predict based on the location measurements 
+# and inferred motions shown below. 
+
+def update(mean1, var1, mean2, var2):
+    new_mean = (var2 * mean1 + var1 * mean2) / (var1 + var2)
+    new_var = 1/(1/var1 + 1/var2)
+    return [new_mean, new_var]
+
+def predict(mean1, var1, mean2, var2):
+    new_mean = mean1 + mean2
+    new_var = var1 + var2
+    return [new_mean, new_var]
+
+measurements = [5., 6., 7., 9., 10.]
+motion = [1., 1., 2., 1., 1.]
+measurement_sig = 4.
+motion_sig = 2.
+mu = 0
+sig = 10000
+
+#Please print out ONLY the final values of the mean
+#and the variance in a list [mu, sig]. 
+
+# Insert code here
+
+print [mu, sig]
