0b8f82d346a3f155e821f75e4d02b18323f2d6f2
AJ <avjmail@gmail.com>
2012-03-05 23:27:44 +0200

    udacity cs373 unit2 22/34

--- computer_science/udacity/cs373/unit2.py
+++ computer_science/udacity/cs373/unit2.py
@@ -103,8 +103,8 @@ motion          X                                           X
 # belief function.
 
 def update(mean1, var1, mean2, var2):
-    new_mean =
-    new_var =
+    new_mean = (1./(var1 + var2))*(var2*mean1 + var1*mean2)
+    new_var = 1./ (1./var1 + 1./var2)
     return [new_mean, new_var]
 
 print update(10.,8.,13., 2.)
