965eabaa08253aa09bb002e085cd4f0b3a54ed94
AJ <avjmail@gmail.com>
2012-03-05 22:57:29 +0200

    udacity cs373 unit2 13/34 init

--- computer_science/udacity/cs373/unit2.py
+++ computer_science/udacity/cs373/unit2.py
@@ -71,3 +71,15 @@ f(x) = 0.2          * e^(-1/2 *           1)
 f(x) = 0.2          * e^ -1/2
 f(x) = 0.2          * .6
 f(x) = 1.2
+
+#13
+#For this problem, you aren't writing any code.
+#Instead, please just change the last argument 
+#in f() to maximize the output.
+
+from math import *
+
+def f(mu, sigma2, x):
+    return 1/sqrt(2.*pi*sigma2) * exp(-.5*(x-mu)**2 / sigma2)
+
+print f(10.,4.,8.) #Change the 8. to something else!
