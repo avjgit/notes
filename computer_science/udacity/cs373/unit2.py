27df3e16e213900c133cd7a424572cc682dcf3a2
AJ <avjmail@gmail.com>
2012-03-05 22:58:18 +0200

    udacity cs373 unit2 13/34

--- computer_science/udacity/cs373/unit2.py
+++ computer_science/udacity/cs373/unit2.py
@@ -82,4 +82,4 @@ from math import *
 def f(mu, sigma2, x):
     return 1/sqrt(2.*pi*sigma2) * exp(-.5*(x-mu)**2 / sigma2)
 
-print f(10.,4.,8.) #Change the 8. to something else!
+print f(10.,4.,10.) #Change the 8. to something else!
