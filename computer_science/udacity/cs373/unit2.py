d25e8197acaa3333cf3ab5704fe346b75094df61
AJ <avjmail@gmail.com>
2012-03-05 23:21:17 +0200

    udacity cs373 unit2 22/34 init

--- computer_science/udacity/cs373/unit2.py
+++ computer_science/udacity/cs373/unit2.py
@@ -89,3 +89,22 @@ print f(10.,4.,10.) #Change the 8. to something else!
                 convolution     product     Bayes rule      Total probability
 measurement                     X           X
 motion          X                                           X
+
+#18 - todo: write formula down?
+# cool:) guessed without calculation
+
+#21 - check calculation
+
+#22
+# Write a program to update your mean and variance
+# when given the mean and variance of your belief
+# and the mean and variance of your measurement.
+# This program will update the parameters of your
+# belief function.
+
+def update(mean1, var1, mean2, var2):
+    new_mean =
+    new_var =
+    return [new_mean, new_var]
+
+print update(10.,8.,13., 2.)
