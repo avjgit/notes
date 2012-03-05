0d692d258f4e11d4058d7207a20725edc844abbc
AJ <avjmail@gmail.com>
2012-03-05 21:00:34 +0200

    udacity cs373 unit2 ex 3/34

--- computer_science/udacity/cs373/unit2.py
+++ computer_science/udacity/cs373/unit2.py
@@ -1,5 +1,9 @@
 # 5 guys and monkey problem
 # 2.3
+#Write a function that takes the number of 
+#coconuts, n, as an argument and returns the
+#number of coconuts after one is given to
+#the monkey and one fifth are taken away.
 def asdf(n):
     return (n-1) * 4./5
 
