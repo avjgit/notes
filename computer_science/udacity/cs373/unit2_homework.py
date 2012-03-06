1c2077ebef3b194e78f3ccde3f5f8c3792704808
AJ <avjmail@gmail.com>
2012-03-06 07:09:43 +0200

    saas homework 1.4 "Rock, Paper, Scissors" init

---
+++ computer_science/udacity/cs373/unit2_homework.py
@@ -0,0 +1,29 @@
+# 1
+# two equal Gaussians.. result sigma will be 
+# > smaller
+# larger
+# same
+
+
+
+# 2
+# two exactly equal Gaussians - sigma will be .5*old_sigma
+
+
+# 3
+# calculate Mu and Sigma for heavy Gaussisan? no
+
+
+# 4
+# dimensions of state vector? 36
+
+
+# 5
+
+
+
+# 6
+
+
+
+# 7
