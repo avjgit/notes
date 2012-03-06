6c568cd411a2ac80c2674a07a78e40c733fbf280
AJ <avjmail@gmail.com>
2012-03-07 00:32:23 +0200

    udacity cs101 uni3 2/44

---
+++ computer_science/udacity/cs101/unit3.py
@@ -0,0 +1,45 @@
+# 01 /44 Introduction
+# 02 /44 Stooges - Question
+stooges = ['Moe', 'Larry', 'Curly']
+# 03 /44 Days In A Month - Question
+# 04 /44 Nested Lists
+# 05 /44 Countries - Question
+# 06 /44 Relative Size - Question
+# 07 /44 Mutation
+# 08 /44 A List Of Strings
+# 09 /44 Different Stooges - Question
+# 10 /44 Yello Mutation
+# 11 /44 Aliasing
+# 12 /44 Secret Agent Man - Question
+# 13 /44 Replace Spy - Question
+# 14 /44 List Operations
+# 15 /44 List Addition And Length
+# 16 /44 Len Quiz - Question
+# 17 /44 Append Quiz - Question
+# 18 /44 How Computers Store Data
+# 19 /44 Dram
+# 20 /44 Memory Hierarchy - Question
+# 21 /44 Hard Drives - Question
+# 22 /44 Loops On Lists - Question
+# 23 /44 For Loops
+# 24 /44 Sum List - Question
+# 25 /44 Measure Udacity - Question
+# 26 /44 Find Element - Question
+# 27 /44 Index - Question
+# 28 /44 Guest Speaker
+# 29 /44 Union - Question
+# 30 /44 Pop
+# 31 /44 Pop Quiz - Question
+# 32 /44 Collecting Links
+# 33 /44 Get All Links
+# 34 /44 Links
+# 35 /44 Starting Get All Links - Question
+# 36 /44 Updating Links - Question
+# 37 /44 Finishing Get All Links - Question
+# 38 /44 Finishing The Web Crawler
+# 39 /44 Crawling Process - Question
+# 40 /44 Crawl Web - Question
+# 41 /44 Crawl Web Loop - Question
+# 42 /44 Crawl If - Question
+# 43 /44 Finishing Crawl Web - Question
+# 44 /44 Conclusion
