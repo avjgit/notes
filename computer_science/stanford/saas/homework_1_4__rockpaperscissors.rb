a8cb289c0e789a57046655b39a9f8ef4af4e3751
AJ <avjmail@gmail.com>
2012-03-06 07:44:16 +0200

    saas homework 1.2 "Rock, Paper, Scissors" part b init

--- computer_science/stanford/saas/homework_1_4__rockpaperscissors.rb
+++ computer_science/stanford/saas/homework_1_4__rockpaperscissors.rb
@@ -52,3 +52,25 @@ end
 #tests
 # puts rps_game_winner([ [ "Armando", "P" ], [ "Dave", "S" ]] )
 # puts rps_game_winner([ [ "Armando", "R" ], [ "Dave", "P" ]] )
+
+
+# (b) A rock, paper, scissors tournament is encoded as a bracketed array of games -
+# that is, each element can be considered its own tournament.
+# [
+# [
+# [ ["Armando", "P"], ["Dave", "S"] ],
+# [ ["Richard", "R"],  ["Michael", "S"] ],
+# ],
+# [ 
+# [ ["Allen", "S"], ["Omer", "P"] ],
+# [ ["David E.", "R"], ["Richard X.", "P"] ]
+# ]
+# ]Under this scenario, Dave would beat Armando (S>P), Richard would beat Michael 
+# (R>S), and then Dave and Richard would play (Richard wins since R>S); similarly, 
+# Allen would beat Omer, David E. would beat Richard X., and Allen and Richard X. 
+# would play (Allen wins since S>P); and finally Richard would beat Allen since R>P, 
+# that is, continue until there is only a single winner.  Write a method 
+# rps_tournament_winner that takes a tournament encoded as a bracketed array 
+# and returns the winner (for the above example, it should return [“Richard”, 
+# “R”]).  You can assume that the array is well formed (that is, there are 2^n players, 
+# and each one participates in exactly one match per round)
