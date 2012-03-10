fde932cac7adff8e470149b6e9bb90cf06b0d452
AJ <avjmail@gmail.com>
2012-03-10 11:51:13 +0200

    udacity cs373 unit3 toc

---
+++ computer_science/udacity/cs373/unit3.py
@@ -0,0 +1,27 @@
+# 1 Field Trip
+# 2 State Space - Question
+# 3 Belief Modality - Question
+# 4 Efficiency - Question
+# 5 Exact Or Approximate - Question
+# 6 Particle Filters
+# 7 Using Robot Class
+# 8 Robot Class Details
+# 9 Moving Robot - Question
+# 10 Add Noise - Question
+# 11 Robot World
+# 12 Creating Particles - Question
+# 13 Robot Particles - Question
+# 14 Importance Weight - Question
+# 15 Resampling - Question
+# 16 Never Sampled 1 - Question
+# 17 Never Sampled 2 - Question
+# 18 Never Sampled 3 - Question
+# 19 New Particle - Question
+# 20 Resampling Wheel - Question
+# 21 Orientation 1 - Question
+# 22 Orientation 2 - Question
+# 23 Error - Question
+# 24 You And Sebastian
+# 25 Filters - Question
+# 26 2012
+# 27 Preview
