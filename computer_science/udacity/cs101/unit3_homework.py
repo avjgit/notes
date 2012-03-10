a93c5052902f3187a0449efcac8268a89f259e1f
AJ <avjmail@gmail.com>
2012-03-10 09:23:48 +0200

    udacity cs101 unit3 hw 3/8 init

--- computer_science/udacity/cs101/unit3_homework.py
+++ computer_science/udacity/cs101/unit3_homework.py
@@ -4,3 +4,13 @@ p[0] = p[0] + p[1]
 p[1] = p[0] + p[2]
 p[2] = p[0] + p[1]
 print p # [1,2,3]
+#2 quiz
+#3
+#Define a procedure, product_list,
+#takes as input a list of numbers,
+#and returns a number that is
+#the result of multiplying all
+#those numbers together.
+
+#product_list([9]) => 9
+#product_list([1,2,3,4]) => 24
