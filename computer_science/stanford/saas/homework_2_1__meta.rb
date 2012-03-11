ca291411bee6ab94a9a018b4f058c0d5d6a51cd8
AJ <avjmail@gmail.com>
2012-03-11 15:46:16 +0200

    saas homework 1a class extension - method in

--- computer_science/stanford/saas/homework_2_1__meta.rb
+++ computer_science/stanford/saas/homework_2_1__meta.rb
@@ -16,16 +16,11 @@
 # 5.dollars.in(:euros)
 # 10.euros.in(:rupees)
 # etc.
-# ● You should support the currencies 'dollars', 'euros', 'rupees' , 'yen' where the
-# conversions are: rupees to dollars, multiply by 0.019; yen to dollars, multiply by 0.013;
-# euro to dollars, multiply by 1.292.
-# ● Both the singular and plural forms of each currency should be acceptable, e.g.
-# 1.dollar.in(:rupees) and 10.rupees.in(:euro) should work.
-# You can use the code shown in lecture as a starting point if you wish; it is at http://pastebin.com/
-# agjb5qBF
 
 class Numeric
   @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1.0, 'lat' => 2.0}
+    # ● Both the singular and plural forms of each currency should be acceptable, e.g.
+    # 1.dollar.in(:rupees) and 10.rupees.in(:euro) should work.
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
@@ -34,9 +29,12 @@ class Numeric
       super
     end
   end
+  def in
+    self * 10
+  end
 end
 
 puts 5.lats
 puts 10.dollar
-puts 11.dollars
-puts 12.lats
+puts 11.dollars.in
+puts 12.lats.in
