4c423ae16281c3486b97bd461652acf23c9aa6e5
AJ <avjmail@gmail.com>
2012-03-11 16:21:10 +0200

    saas homework 1c init

--- computer_science/stanford/saas/homework_2_1__meta.rb
+++ computer_science/stanford/saas/homework_2_1__meta.rb
@@ -60,3 +60,11 @@ class String
       part1 ==  part2           #compare first half to second
     end
 end
+
+#c
+# c) Adapt your palindrome solution so that it works on Enumerables. That is:
+# [1,2,3,2,1].palindrome? # => true
+# (It's not necessary for the collection's elements to be palindromes themselves--only that the
+# top-level collection be a palindrome.) HINT: this should require fewer than 5 lines of code.
+# Although hashes are considered Enumerables, your solution does not need to make sense for
+# hashes (though it should not error).
