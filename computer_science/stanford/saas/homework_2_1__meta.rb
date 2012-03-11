4ac402c2018ee54292a8a72eb7757bc685cd30c5
AJ <avjmail@gmail.com>
2012-03-11 15:37:40 +0200

    saas homework 1a class extension - lvl, usd plural

--- computer_science/stanford/saas/homework_2_1__meta.rb
+++ computer_science/stanford/saas/homework_2_1__meta.rb
@@ -13,7 +13,7 @@
 # agjb5qBF
 
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1.0, 'lat' => 2.0}
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
@@ -25,9 +25,11 @@ class Numeric
 end
 
 class Numeric
-    def lats ; self * 2 ; end
-    def dollar ; self ; end
+    # def lats ; self * 2 ; end
+    # def dollar ; self ; end
 end
 
 puts 5.lats
 puts 10.dollar
+puts 11.dollars
+puts 12.lats
