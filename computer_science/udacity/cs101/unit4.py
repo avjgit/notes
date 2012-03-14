5fb4e310d4e671725e9a50838a377776a8a08b92
AJ <avjmail@gmail.com>
2012-03-14 22:49:44 +0200

    udacity cs101 unit4 toc

---
+++ computer_science/udacity/cs101/unit4.py
@@ -0,0 +1,22 @@
+﻿UNIT 4
+01/ 21 Introduction
+02/ 21 Data Structures - Question
+03/ 21 Add To Index - Question
+04/ 21 Lookup - Question
+05/ 21 Building The Web Index
+06/ 21 Add Page To Index - Question
+07/ 21 Finishing The Web Crawler - Question
+08/ 21 Startup
+09/ 21 The Internet
+10/ 21 Networks - Question
+11/ 21 Smoke Signals
+12/ 21 Latency - Question
+13/ 21 Bandwidth
+14/ 21 Bits - Question
+15/ 21 Buckets Of Bits
+16/ 21 What Is Your Bandwidth - Question
+17/ 21 Traceroute
+18/ 21 Traveling Data - Question
+19/ 21 Making A Network
+20/ 21 Protocols
+21/ 21 Conclusion
