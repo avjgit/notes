98c0c555db72400fa771fb585389f465162357d9
AJ <avjmail@gmail.com>
2012-03-19 08:06:47 +0200

    udacity cs101 6/21 init

--- computer_science/udacity/cs101/unit4.py
+++ computer_science/udacity/cs101/unit4.py
@@ -64,6 +64,35 @@ def lookup(index,keyword):
 string.split
 
 # 06/ 21 Add Page To Index - Question
+#Define a procedure, add_page_to_index,
+#that takes three inputs:
+
+#   - index
+#   - url (String)
+#   - content (String)
+
+#It should update the index to include
+#all of the word occurences found in the
+#page content by adding the url to the
+#word's associated url list.
+
+
+index = []
+
+#add_page_to_index(index,'fake.text',"This is a test")
+#print index => [['This', ['fake.text']], ['is', ['fake.text']], ['a', ['fake.text']], ['test', ['fake.text']]]
+
+def add_to_index(index,keyword,url):
+    for entry in index:
+        if entry[0] == keyword:
+            entry[1].append(url)
+            return
+    index.append([keyword,[url]])
+
+def add_page_to_index(index,url,content):
+
+
+
 # 07/ 21 Finishing The Web Crawler - Question
 # 08/ 21 Startup
 # 09/ 21 The Internet
