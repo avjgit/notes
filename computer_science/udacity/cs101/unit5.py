7f9497c3c42c243c20fca96a4ae1a91d5ced3113
AJ <avjmail@gmail.com>
2012-03-22 06:32:53 +0200

    udacity cs101 unit 5 toc

---
+++ computer_science/udacity/cs101/unit5.py
@@ -0,0 +1,34 @@
+﻿# 01/ 34 Introduction
+# 02/ 34 Making Things Fast
+# 03/ 34 Measuring Speed - Question
+# 04/ 34 Stopwatch
+# 05/ 34 Spin Loop
+# 06/ 34 Predicting Run Time - Question
+# 07/ 34 Make Big Index
+# 08/ 34 Index Size Vs. Time - Question
+# 09/ 34 Lookup Time - Question
+# 10/ 34 Worst Case - Question
+# 11/ 34 Fast Enough - Question
+# 12/ 34 Making Lookup Faster
+# 13/ 34 Hash Table - Question
+# 14/ 34 Hash Function
+# 15/ 34 Modulus Operator
+# 16/ 34 Modulus Quiz - Question
+# 17/ 34 Equivalent Expressions - Question
+# 18/ 34 Bad Hash - Question
+# 19/ 34 Better Hash Functions - Question
+# 20/ 34 Testing Hash Functions
+# 21/ 34 Keywords And Buckets - Question
+# 22/ 34 Implementing Hash Tables - Question
+# 23/ 34 Empty Hash Table - Question
+# 24/ 34 The Hard Way - Question
+# 25/ 34 Finding Buckets - Question
+# 26/ 34 Adding Keywords - Question
+# 27/ 34 Lookup - Question
+# 28/ 34 Update - Question
+# 29/ 34 Dictionaries
+# 30/ 34 Using Dictionaries
+# 31/ 34 Population - Question
+# 32/ 34 A Noble Gase
+# 33/ 34 Modifying The Search Engine - Question
+# 34/ 34 Changing Lookup - Question
