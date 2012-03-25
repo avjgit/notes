e31c4171c4c8c0deeb7c5396a3ac2b0d54110c48
AJ <avjmail@gmail.com>
2012-03-26 00:28:24 +0300

    algoI toc

---
+++ computer_science/stanford/algorithms_I/notes.py
@@ -0,0 +1,42 @@
+I. INTRODUCTION
+Introduction : Why Study Algorithms ? (19 min)     
+About the Course (17 min)     
+Merge Sort: Motivation and Example (9 min)     
+Merge Sort: Pseudocode (13 min)     
+Merge Sort: Analysis (9 min)     
+Guiding Principles for Analysis of Algorithms (15 min)     
+
+II. ASYMPTOTIC ANALYSIS
+Big-Oh Notation (4 min)     
+Basic Examples (7 min)     
+Big Omega and Theta (7 min)     
+Additional Examples [Review - Optional] (8 min)     
+
+III. DIVIDE & CONQUER ALGORITHMS
+O(n log n) Algorithm for Counting Inversions I (13 min)     
+O(n log n) Algorithm for Counting Inversions II (17 min)     
+Strassen''s Subcubic Matrix Multiplication Algorithm (22 min)     
+O(n log n) Algorithm for Closest Pair I [Advanced - Optional] (32 min)     
+O(n log n) Algorithm for Closest Pair II [Advanced - Optional] (19 min)     
+
+IV. THE MASTER METHOD
+Motivation (8 min)     
+Formal Statement (10 min)     
+Examples (13 min)     
+Proof I (10 min)     
+Interpretation of the 3 Cases (11 min)     
+Proof II (16 min)     
+
+V. QUICKSORT - ALGORITHM
+Quicksort: Overview (12 min)     
+Partitioning Around a Pivot (25 min)     
+Correctness of Quicksort [Review - Optional] (11 min)     
+Choosing a Good Pivot (22min)     
+
+VI. QUICKSORT - ANALYSIS
+Analysis I: A Decomposition Principle [Advanced - Optional] (22 min)     
+Analysis II: The Key Insight [Advanced - Optional] (12min)     
+Analysis III: Final Calculations [Advanced - Optional] (9min)      
+
+VII. PROBABILITY REVIEW
+Part I [Review - Optional] (25 min)
