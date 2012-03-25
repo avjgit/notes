be64664420a5f59a1c7bd0b808d005b439d1654b
AJ <avjmail@gmail.com>
2012-03-25 07:02:27 +0300

    ucadicy cs101 unit5 25/34 init

--- computer_science/udacity/cs101/unit5.py
+++ computer_science/udacity/cs101/unit5.py
@@ -84,6 +84,29 @@ def make_hashtable(nbuckets):
 
 # 24/ 34 The Hard Way - Question
 # 25/ 34 Finding Buckets - Question
+#Define a procedure, hashtable_get_bucket,
+#that takes two inputs - a hashtable, and
+#a keyword, and outputs the bucket where the
+#keyword could occur.
+
+#hash_string(keyword,nbuckets) => index of bucket
+
+def hashtable_get_bucket(htable,keyword):
+    
+
+
+def hash_string(keyword,buckets):
+    out = 0
+    for s in keyword:
+        out = (out + ord(s)) % buckets
+    return out
+
+def make_hashtable(nbuckets):
+    table = []
+    for unused in range(0,nbuckets):
+        table.append([])
+    return table
+
 # 26/ 34 Adding Keywords - Question
 # 27/ 34 Lookup - Question
 # 28/ 34 Update - Question
