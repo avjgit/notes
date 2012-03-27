476c3ade9f754e831d5b7eb2fe3d723bb21c5ae9
AJ <avjmail@gmail.com>
2012-03-27 22:09:45 +0300

    udacity cs101 unit6 toc

---
+++ computer_science/udacity/cs101/unit6.py
@@ -0,0 +1,29 @@
+# 01 Infinite Power
+# 02 Long Words - Question
+# 03 Counter
+# 04 Counter Quiz - Question
+# 05 Expanding Our Grammar - Question
+# 06 Recursive Definitions
+# 07 Ancestors - Question
+# 08 Recursive Procedures
+# 09 Recursive Factorial - Question
+# 10 Palindromes - Question
+# 11 Recursive Vs Iterative
+# 12 Bunnies - Question
+# 13 Divide And Be Conquered
+# 14 Counting Calls - Question
+# 15 Faster Fibonacci - Question
+# 16 Ranking Web Pages
+# 17 Popularity
+# 18 Good Definitions - Question
+# 19 Circular Definitions - Question
+# 20 Relaxation - Question
+# 21 Page Rank
+# 22 Altavista - Question
+# 23 Urank
+# 24 Implementing Urank - Question
+# 25 Computing Page Rank
+# 26 Formal Calculations
+# 27 Computer Ranks
+# 28 Finishing Urank - Question
+# 29 Search Engine
