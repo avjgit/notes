33e70ab0028aa668241b09247be3bdeaae3f064f
AJ <avjmail@gmail.com>
2012-03-29 19:27:52 +0300

    algorithms: mergesort: fix

--- computer_science/stanford/algorithms_I/sort__merge.py
+++ computer_science/stanford/algorithms_I/sort__merge.py
@@ -1,17 +1,28 @@
-sorted1 = [1,2,3]
-sorted2 = [10,20,30]
-
 def sort_merge(a1, a2):
+    
     result = []
-    n = len(sorted1) + len(sorted2)
+    
+    n = len(a1) + len(a2)
+    
     i, j = 0, 0
+    
     for k in range(n):
-        if a1[i] < a2[j]:
+        if (i < len(a1)) and (a1[i] < a2[j]):
             result.append(a1[i])
             i = i + 1
         else:
             result.append(a2[j])
             j = j + 1
     return result
-    
+
+sorted1 = [1,2,3]
+sorted2 = [10,20,30]
+print(sort_merge(sorted1,sorted2))
+
+sorted1 = [1,3,5]
+sorted2 = [2,4,6]
+print(sort_merge(sorted1,sorted2))
+
+sorted1 = [1,4,8]
+sorted2 = [2,3,9]
 print(sort_merge(sorted1,sorted2))
