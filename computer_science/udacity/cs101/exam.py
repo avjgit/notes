478f6b51b625c89b22b5abb2fa1acc5602f189a1
AJ <avjmail@gmail.com>
2012-04-08 21:34:20 +0300

    udacity cs101 exam init

---
+++ computer_science/udacity/cs101/exam.py
@@ -0,0 +1,11 @@
+# 01 Grammars
+# 02 Procedures And If
+# 03 Prefix Removal
+# 04 Collatz Returns
+# 05 Cost
+# 06 List Explosion
+# 07 Reverse Index
+# 08 Same Structure
+# 09 Reachability
+# 10 Spelling Correction
+# 11 Multiword Queries
