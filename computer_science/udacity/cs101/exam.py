d8a7ce8f870aaf2d2eab2214bbc0d9d0a9d597ba
AJ <avjmail@gmail.com>
2012-04-08 21:41:09 +0300

    udacity cs101 exam 2/11

--- computer_science/udacity/cs101/exam.py
+++ computer_science/udacity/cs101/exam.py
@@ -1,28 +1,27 @@
 # 01 Grammars
-How many different strings can the following grammar generate, starting from Word?
-Word → udacious
-Word → super Word
-:: I think, infinitely many
+# How many different strings can the following grammar generate, starting from Word?
+# Word → udacious
+# Word → super Word
+# :: I think, infinitely many
 
 # 02 Procedures And If
-#Procedures and If
-
 #Define a procedure, unique, that takes three numbers as its inputs and returns
 #the Boolean value True if all three numbers are different. Otherwise, it should
 #return the Boolean value False.
 
 def unique(a, b, c):
+    return not (a == b or b == c or a == c)
 
 
 #For example,
 
-#print unique(1, 2, 3)
+print (unique(1, 2, 3))
 #>>> True
 
-#print unique(1, 0, 1)
+print (unique(1, 0, 1))
 #>>> False
 
-#print unique(7, 7, 7)
+print (unique(7, 7, 7))
 #>>> False
 
 # 03 Prefix Removal
