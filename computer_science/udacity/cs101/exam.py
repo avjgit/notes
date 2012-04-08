a5b273b02a5ba24a83460291c3725782f2406af5
AJ <avjmail@gmail.com>
2012-04-08 22:01:04 +0300

    udacity cs101 exam 6/11 init

--- computer_science/udacity/cs101/exam.py
+++ computer_science/udacity/cs101/exam.py
@@ -115,6 +115,28 @@ def tricky_loop(p):
     return 101 # so, total length looks like 2p.. so, linearly
 
 # 06 List Explosion
+#List Explosion
+
+
+#Define a procedure, explode_list, that takes as inputs a list and a number, n.
+#It should return a list which contains each of the elements of the input list,
+#in the original order, but repeated n times.
+
+def explode_list(p,n):
+
+
+
+#For example,
+
+#print explode_list([1, 2, 3], 2)
+#>>> [1, 1, 2, 2, 3, 3]
+
+#print explode_list([1, 0, 1], 0)
+#>>> []
+
+#print explode_list(["super"], 5)
+#>>> ["super", "super", "super", "super", "super"]
+
 # 07 Reverse Index
 # 08 Same Structure
 # 09 Reachability
