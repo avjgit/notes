36c9e5c5a59ca25fd9561591e237c987c1b0859f
AJ <avjmail@gmail.com>
2012-04-08 18:09:03 +0300

    udacity cs101 unit6 hw 2/5 fix

--- computer_science/udacity/cs101/unit6_homework.py
+++ computer_science/udacity/cs101/unit6_homework.py
@@ -29,7 +29,7 @@ def rabbits(n):
     if n == 1:
         return 1
     if n == 2:
-        return 2
+        return 1
     if n <= 5:
         return rabbits(n-1) + rabbits(n-2)
     return rabbits(n-1) + rabbits(n-2) - rabbits(n-5)
@@ -39,11 +39,11 @@ print(rabbits(10))
 # print(rabbits(10))
 #>>> 35
 
-#s = ""
-#for i in range(1,12):
-#    s = s + str(rabbits(i)) + " "
-#print s
-#>>> 1 1 2 3 5 7 11 16 24 35 52 
+s = ""
+for i in range(1,12):
+   s = s + str(rabbits(i)) + " "
+print(s)
+# >>> 1 1 2 3 5 7 11 16 24 35 52 
 
     
  
