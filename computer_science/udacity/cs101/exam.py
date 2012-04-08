85ebb904b958c9484750355237003c13a5f24265
AJ <avjmail@gmail.com>
2012-04-08 21:35:06 +0300

    udacity cs101 exam 2/11 init

--- computer_science/udacity/cs101/exam.py
+++ computer_science/udacity/cs101/exam.py
@@ -5,6 +5,26 @@ Word → super Word
 :: I think, infinitely many
 
 # 02 Procedures And If
+#Procedures and If
+
+#Define a procedure, unique, that takes three numbers as its inputs and returns
+#the Boolean value True if all three numbers are different. Otherwise, it should
+#return the Boolean value False.
+
+def unique(a, b, c):
+
+
+#For example,
+
+#print unique(1, 2, 3)
+#>>> True
+
+#print unique(1, 0, 1)
+#>>> False
+
+#print unique(7, 7, 7)
+#>>> False
+
 # 03 Prefix Removal
 # 04 Collatz Returns
 # 05 Cost
