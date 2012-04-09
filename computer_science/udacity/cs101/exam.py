945ed570ed567d3dcaf2549a01aaeb0c901d0a30
AJ <avjmail@gmail.com>
2012-04-09 10:39:04 +0300

    udacity cs101 exam 9/11 init

--- computer_science/udacity/cs101/exam.py
+++ computer_science/udacity/cs101/exam.py
@@ -210,21 +210,52 @@ def same_structure(a,b):
 
 #Here are some examples:
 
-print(same_structure(3, 7))
+# print(same_structure(3, 7))
 #>>> True
 
-print(same_structure([1, 0, 1], [2, 1, 2]))
+# print(same_structure([1, 0, 1], [2, 1, 2]))
 #>>> True
 
-print(same_structure([1, [0], 1], [2, 5, 3]))
+# print(same_structure([1, [0], 1], [2, 5, 3]))
 #>>> False
 
-print(same_structure([1, [2, [3, [4, 5]]]], ['a', ['b', ['c', ['d', 'e']]]]))
+# print(same_structure([1, [2, [3, [4, 5]]]], ['a', ['b', ['c', ['d', 'e']]]]))
 #>>> True
 
-print(same_structure([1, [2, [3, [4, 5]]]], ['a', ['b', ['c', ['de']]]]))
+# print(same_structure([1, [2, [3, [4, 5]]]], ['a', ['b', ['c', ['de']]]]))
 #>>> False
 
 # 09 Reachability
+#Single Gold Star
+
+#Define a procedure, reachable(graph, node), that takes as input a graph and a
+#starting node, and returns a list of all the nodes in the graph that can be
+#reached by following zero or more edges starting from node.  The input graph is
+#represented as a Dictionary where each node in the graph is a key in the
+#Dictionary, and the value associated with a key is a list of the nodes that the
+#key node is connected to.  The nodes in the returned list may appear in any
+#order, but should not contain any duplicates.
+
+
+def reachable(graph, node):
+    return True
+
+
+
+
+
+#For example,
+
+graph = {'a': ['b', 'c'], 'b': ['a', 'c'], 'c': ['b', 'd'], 'd': ['a'], 'e': ['a']}
+
+#print reachable(graph, 'a')
+#>>> ['a', 'c', 'd', 'b']
+
+#print reachable(graph, 'd')
+#>>> ['d', 'a', 'c', 'b']
+
+#print reachable(graph, 'e')
+#>>> ['e', 'a', 'c', 'd', 'b']
+
 # 10 Spelling Correction
 # 11 Multiword Queries
