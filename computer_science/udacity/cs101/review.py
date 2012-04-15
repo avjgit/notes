f45d0ecc0b5da820e47a46620e6f39dab5b8aa3e
AJ <avjmail@gmail.com>
2012-04-15 11:59:54 +0300

    udacity cs101 review task with answer

--- computer_science/udacity/cs101/review.py
+++ computer_science/udacity/cs101/review.py
@@ -55,6 +55,33 @@
             table.append([])
         return table
 
+    #Define a procedure,
+
+    #hashtable_add(htable,key,value)
+
+    #that adds the key to the hashtable
+    #(in the correct bucket), with the
+    #correct value.
+
+    def hashtable_add(htable,key,value):
+        bucket = hashtable_get_bucket(htable,keyword)
+        htable[bucket].append([key, [value]])
+
+    def hashtable_get_bucket(htable,keyword):
+        return htable[hash_string(keyword,len(htable))]
+
+    def hash_string(keyword,buckets):
+        out = 0
+        for s in keyword:
+            out = (out + ord(s)) % buckets
+        return out
+
+    def make_hashtable(nbuckets):
+        table = []
+        for unused in range(0,nbuckets):
+            table.append([])
+        return table
+
 # hw      5 
 # unit    6 
 # hw      6 
