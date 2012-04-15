f062613eeebbc61fe32f16ca6c42da22cf20e42d
AJ <avjmail@gmail.com>
2012-04-15 10:20:22 +0300

    udacity cs101 review toc

---
+++ computer_science/udacity/cs101/review.py
@@ -0,0 +1,15 @@
+# unit    1 100
+# hw      1 
+# unit    2 
+# hw      2 
+# unit    3 
+# hw      3 
+# unit    4 
+# hw      4 
+# unit    5 
+# hw      5 
+# unit    6 
+# hw      6 
+# unit    7 
+# hw      7 
+# exam    
