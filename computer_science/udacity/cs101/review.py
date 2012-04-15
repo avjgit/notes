f81581634871442852b8ab3950eb4ae650d85b49
AJ <avjmail@gmail.com>
2012-04-15 10:28:03 +0300

    udacity cs101 review task with answer

--- computer_science/udacity/cs101/review.py
+++ computer_science/udacity/cs101/review.py
@@ -1,13 +1,20 @@
 # unit    1 100
-# hw      1 
+# hw      1 89
     given any string s, which of following 
     has always same value as s?
     > a ('a' + s) [1:] #yes, of course (facepalm)
     > b s + ''
     c s[0] + s[1:] # fails for empty?
     d s[0:] #looks like ok..
-# unit    2 
-# hw      2 
+# unit    2 100
+# hw      2 86
+    # does this loop finishes? :: no...
+    n = any positive integer
+    while n != 1:
+        if n % 2 == 0: # the % means remainder, so this tests if n is even
+            n = n / 2
+        else:
+            n = 3 * n  +  1
 # unit    3 
 # hw      3 
 # unit    4 
