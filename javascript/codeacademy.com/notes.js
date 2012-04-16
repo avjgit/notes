e0cbed5ddc2f479b671f0dbbb25bcea5739c07f9
AJ <avjmail@gmail.com>
2012-04-16 17:54:44 +0300

    javascript: codeacademy: unit 7: section 1: ex 4/5; methods

--- javascript/codeacademy.com/notes.js
+++ javascript/codeacademy.com/notes.js
@@ -2179,6 +2179,33 @@ property = "people";
 
 console.log(jordan[property]);
 
+var australia = {
+    hemisphere: "southern",
+    population: function(newArrivals) {
+        return 20000000 + newArrivals;
+    }
+};
+
+var australia2 = new Object();
+australia2.hemisphere = "southern";
+australia2.population = function(newArrivals){
+    return 20000000 + newArrivals;
+};
+
+// create your objects and methods here
+var me = {
+    planet: "Earth",
+    age: function(yearBorn){
+        return 2012 - yearBorn;
+    }
+}
+
+var jeremyLin = new Object();
+jeremyLin.team = "Knicks";
+jeremyLin.playGame = function(opponent){
+    return "Tonight the Knicks square off against the " + opponent;
+}
+
 
 
 
