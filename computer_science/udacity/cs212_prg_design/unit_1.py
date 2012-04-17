b0caa98bea8bdd0ee7a72c12fcea6503e4dbc629
AJ <avjmail@gmail.com>
2012-04-17 14:13:04 +0300

    cs212:unit1:lessons:ex_of_29:6 init

--- computer_science/udacity/cs212_prg_design/unit_1.py
+++ computer_science/udacity/cs212_prg_design/unit_1.py
@@ -11,3 +11,20 @@ combinations:
     n of a kind (n of same of rank)
     straight (sequence)
     flush (same suit)
+
+# -----------
+# User Instructions
+# 
+# Modify the poker() function to return the best hand 
+# according to the hand_rank() function. Since we have
+# not yet written hand_rank(), clicking RUN won't do 
+# anything, but clicking SUBMIT will let you know if you
+# have gotten the problem right. 
+#
+
+def poker(hands):
+    "Return the best hand: poker([hand,...]) => hand"
+    return max(#enter your code here. Your return should call max())
+
+def hand_rank(hand):
+    return None # we will be changing this later.    
