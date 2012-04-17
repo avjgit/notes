5db123a7b84b49f77d729fa72e34e06fe352bd75
AJ <avjmail@gmail.com>
2012-04-17 13:58:58 +0300

    cs212:unit1:lessons:ex_of_29:2

---
+++ computer_science/udacity/cs212_prg_design/unit_1.py
@@ -0,0 +1,13 @@
+domain: poker
+    hand
+    card
+    rank
+    suit
+
+expected functions:
+    poker(hands) -> besthand
+
+combinations:
+    n of a kind (n of same of rank)
+    straight (sequence)
+    flush (same suit)
