102298da3743f3e93e7a1ffd3865250e0b2f4b44
AJ <avjmail@gmail.com>
2012-04-17 19:14:25 +0300

    cs212:unit1:lessons:ex_of_29:16 fix

--- computer_science/udacity/cs212_prg_design/unit_1.py
+++ computer_science/udacity/cs212_prg_design/unit_1.py
@@ -146,19 +146,7 @@ tuple(7, 9, 5) # like immutable set
 
 def card_ranks(cards):
     "Return a list of the ranks, sorted with higher first."
-    ranks = [r for r,s in cards]
-    for i in range(0, len(ranks)):
-        if ranks[i] == 'T':
-            ranks[i] = 10
-        elif ranks[i] == 'J':
-            ranks[i] = 11
-        elif ranks[i] == 'Q':
-            ranks[i] = 12
-        elif ranks[i] == 'K':
-            ranks[i] = 13
-        elif ranks[i] == 'A':
-            ranks[i] = 14
-        ranks[i] = int(ranks[i])
+    ranks = ['--23456789TJQKA'.index(r) for r,s in cards]
     ranks.sort(reverse=True)
     return ranks
 
